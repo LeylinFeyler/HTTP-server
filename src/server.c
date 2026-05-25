a11f1ace2c1f0f82c182baba8942a1f1c67dd9f1
Vlad Voloshchuk <vladuslav0811@gmail.com>
2026-05-25 20:04:21 +0300

    add keep-alive idle timeout

--- src/server.c
+++ src/server.c
@@ -11,9 +11,14 @@
 #include <string.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
+#include <time.h>
 #include <unistd.h>
 
 #define MAX_EVENTS 64
+#define MAX_CLIENTS 65536
+#define KEEP_ALIVE_TIMEOUT 5
+
+time_t client_last_activity[MAX_CLIENTS] = {0};
 
 void handle_client(int client_fd, struct sockaddr_in *client) {
     char ip[INET_ADDRSTRLEN];
@@ -33,11 +38,13 @@ void handle_client(int client_fd, struct sockaddr_in *client) {
         buffer[n] = '\0';
         strcpy(raw_buffer, buffer);
 
-        HttpRequest req;
+        HttpRequest req = {0};
         if (!parse_request(buffer, &req)) {
             send_error_page(client_fd, 400, "Bad Request", 0);
             break;
         }
+        /* refresh timeout */
+        client_last_activity[client_fd] = time(NULL);
 
         int keep_alive = should_keep_alive(&req);
         int send_body  = 1;
@@ -60,10 +67,11 @@ void handle_client(int client_fd, struct sockaddr_in *client) {
             send_file(client_fd, req.path, send_body, keep_alive);
         }
 
-        /* close requested */
+        /* client requested close */
         if (!keep_alive) {
             break;
         }
+        break;
     }
 }
 
@@ -76,21 +84,35 @@ void run_server_epoll(void) {
         exit(1);
     }
 
-    struct epoll_event ev;
-    memset(&ev, 0, sizeof(ev));
-    ev.events  = EPOLLIN;
-    ev.data.fd = server_fd;
+    struct epoll_event ev = {0};
+    ev.events             = EPOLLIN;
+    ev.data.fd            = server_fd;
 
     if (epoll_ctl(epfd, EPOLL_CTL_ADD, server_fd, &ev) < 0) {
         perror("epoll_ctl");
         exit(1);
     }
 
-    struct epoll_event events[MAX_EVENTS];
-    memset(&events, 0, sizeof(events));
+    struct epoll_event events[MAX_EVENTS] = {0};
 
     while (1) {
-        int nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
+        /* cleanup idle clients */
+        time_t now = time(NULL);
+
+        for (int fd = 0; fd < MAX_CLIENTS; fd++) {
+            if (client_last_activity[fd] == 0) {
+                continue;
+            }
+
+            if ((now - client_last_activity[fd]) > KEEP_ALIVE_TIMEOUT) {
+                printf("fd %d timeout\n", fd);
+                epoll_ctl(epfd, EPOLL_CTL_DEL, fd, NULL);
+                close(fd);
+                client_last_activity[fd] = 0;
+            }
+        }
+
+        int nfds = epoll_wait(epfd, events, MAX_EVENTS, 1000);
         if (nfds < 0) {
             perror("epoll_wait");
             continue;
@@ -99,12 +121,16 @@ void run_server_epoll(void) {
         for (int i = 0; i < nfds; i++) {
             int current_fd = events[i].data.fd;
 
+            /* new client */
             if (current_fd == server_fd) {
-                struct sockaddr_in client;
+                struct sockaddr_in client = {0};
+
                 int client_fd = accept_client(server_fd, &client);
                 if (client_fd < 0) {
                     continue;
                 }
+
+                client_last_activity[client_fd] = time(NULL);
 
                 ev.events  = EPOLLIN;
                 ev.data.fd = client_fd;
@@ -123,9 +149,15 @@ void run_server_epoll(void) {
 
             getpeername(current_fd, (struct sockaddr *)&client, &len);
             handle_client(current_fd, &client);
-            epoll_ctl(epfd, EPOLL_CTL_DEL, current_fd, NULL);
 
-            close(current_fd);
+            /* closed connection */
+            char test;
+
+            if (recv(current_fd, &test, 1, MSG_PEEK | MSG_DONTWAIT) == 0) {
+                epoll_ctl(epfd, EPOLL_CTL_DEL, current_fd, NULL);
+                close(current_fd);
+                client_last_activity[current_fd] = 0;
+            }
         }
     }
 }
